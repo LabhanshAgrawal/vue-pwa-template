d49f13e07d9e7eec8d0ac5b1bda8de898b7f72a9
Toilal <toilal.dev@gmail.com>
2017-05-15 23:43:28 +0200

    Add vue-property-decorator in TypeScript boilerplate
    Close #19

--- template/src/components/Hello.vue.js
+++ template/src/components/Hello.vue.js
@@ -2,6 +2,16 @@ import withRender from './Hello.html?style=./Hello.css'{{#if_eq eslintConfig "ai
 
 export default withRender({
   name: 'hello',
+  props: {
+    links: {
+      type: Boolean,
+      default: true{{#if_eq eslintConfig "airbnb"}},{{/if_eq}}
+    },
+    ecosystem: {
+      type: Boolean,
+      default: true{{#if_eq eslintConfig "airbnb"}},{{/if_eq}}
+    }{{#if_eq eslintConfig "airbnb"}},{{/if_eq}}
+  },
   data{{#unless_eq eslintConfig "airbnb"}} {{/unless_eq}}() {
     return {
       msg: 'Welcome to Your Vue.js App'{{#if_eq eslintConfig "airbnb"}},{{/if_eq}}
